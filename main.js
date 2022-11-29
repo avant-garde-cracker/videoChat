396f0da6851e655a9450efce73b6f6bb4bed06bd
avant-garde-cracker <47210155+avant-garde-cracker@users.noreply.github.com>
2022-11-29 07:33:41 +0530

    video chat application

---
+++ main.js
@@ -0,0 +1,98 @@
+// Sign up to agro.io to generate an APP ID
+const APP_ID = "<YOUR APP ID>"
+let TOKEN = null
+let CHANNEL = "main"
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+let localTracks = []
+let remoteUsers = {}
+let urlParmas  = new URLSearchParams(window.location.search)
+let room = urlParmas.get('room')
+console.log('Urm urlParmas: ', urlParmas)
+console.log('Room: ', room)
+
+let joinAndDisplayLocalStream = async () => {
+	client.on('user-published', handleUserJoined)
+	client.on('user-left', handleUserLeft)
+	let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+	console.log('UID ', UID)
+
+	localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+	let player  = `<div class="video-container" id="user-conatiner-${UID}">
+						<div class="video-player" id="user-${UID}"></div> 
+				   </div>`
+	document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+	localTracks[1].play(`user-${UID}`)
+	await client.publish([localTracks[0], localTracks[1]])
+}
+
+let joinStream = async () => {
+	await joinAndDisplayLocalStream()
+	document.getElementById('join-btn').style.display = 'none'
+	document.getElementById('stream-controls').style.display = 'flex'
+}
+
+let handleUserJoined = async (user, mediaType) => {
+	remoteUsers[user.id] = user
+	await client.subscribe(user, mediaType)
+
+	if(mediaType === 'video') {
+		let player = document.getElementById(`user-conatiner-${user.uid}`)
+		if(player != null) {
+			player.remove()
+		}
+	player  = `<div class="video-container" id="user-conatiner-${user.uid}">
+						<div class="video-player" id="user-${user.uid}"></div> 
+				   </div>`
+	document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+	user.videoTrack.play(`user-${user.uid}`)
+   }
+
+   if(mediaType === 'audio'){
+   		user.audioTrack.play()
+   }
+}
+
+let handleUserLeft = async (user) => {
+	delete remoteUsers[user.uid]
+	document.getElementById(`user-conatiner-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () => {
+	for(let i = 0; localTracks.length > i; i++){
+		localTracks[i].stop()
+		localTracks[i].close()
+	}
+	await client.leave()
+	document.getElementById('join-btn').style.display = 'block'
+	document.getElementById('stream-controls').style.display = 'none'
+	document.getElementById('video-streams').innerHTML = ''
+}
+
+let toggleMic = async (e) => {
+	if(localTracks[0].muted){
+		await localTracks[0].setMuted(false)
+		e.target.innerHTML = 'Mic on'
+		e.target.style.backgroundColor = 'cadetblue'
+	} else{
+		await localTracks[0].setMuted(true)
+		e.target.innerHTML = 'Mic Off'
+		e.target.style.backgroundColor = '#EE4B2B'
+	}
+}
+
+let toggleCamera = async (e) => {
+	if(localTracks[1].muted){
+		await localTracks[1].setMuted(false)
+		e.target.innerHTML = 'Camera on'
+		e.target.style.backgroundColor = 'cadetblue'
+	} else{
+		await localTracks[1].setMuted(true)
+		e.target.innerHTML = 'Camera Off'
+		e.target.style.backgroundColor = '#EE4B2B'
+	}
+}
+
+document.getElementById('join-btn').addEventListener('click', joinStream)
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
